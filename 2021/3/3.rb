8764393d278637bf4972f4895fb7e5a4143cb57f
Jon Ryer <jonathan.ryer@gmail.com>
2021-12-02 23:51:06 -0600

    add day 3 solution

---
+++ 2021/3/3.rb
@@ -0,0 +1,42 @@
+lines = File.readlines('input').map{|x| x.to_s.strip}
+
+total_lines = lines.size
+num_bits = lines.first.size
+output = Array.new(num_bits)
+
+lines.each.with_index do |line|
+    chars = line.strip.split('')
+    chars.each_with_index do |x, j| 
+        output[j] ||= 0
+        output[j] += x.to_i
+    end
+end
+
+gamma = ''
+episilon = ''
+output.each do |i|
+    num = (i.to_f / total_lines).round
+    gamma += num.to_s
+    episilon += (1 - num).to_s
+end
+puts gamma.to_i(2) *  episilon.to_i(2)
+
+####
+
+bit = 0
+oxy = lines.clone
+co2 = lines.clone
+while bit < num_bits
+    avg_oxy = oxy.sum{ |x| x[bit].to_i }.to_f / oxy.size
+    avg_co2 = co2.sum{ |x| x[bit].to_i }.to_f / co2.size
+    
+    keep_oxy = avg_oxy >= 0.5 ? 1 : 0
+    keep_co2 =  (avg_co2 >= 0.5 ? 0 : 1)
+    
+    oxy.filter! { |x| x[bit].to_i == keep_oxy } if oxy.size > 1
+    co2.filter! {|x| x[bit].to_i == keep_co2 } if co2.size > 1
+    bit = bit + 1
+end
+oxy = oxy[0].to_i(2)
+co2 = co2[0].to_i(2)
+puts oxy * co2
