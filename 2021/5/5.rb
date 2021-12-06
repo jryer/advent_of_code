4c171bc4af777f5e15ecec9942848635713c7429
Jon Ryer <jonathan.ryer@gmail.com>
2021-12-05 23:33:31 -0600

    day 6 solution

--- 2021/5/5.rb
+++ 2021/5/5.rb
@@ -1,91 +1,85 @@
 class Input
-    attr_accessor :x1, :x2, :y1, :y2
-    #
-    # @param [<Type>] input String input in format x1,y1 -> x2,y2
-    #
-    def initialize(input)
-        @input = input.strip
-        parsed_inputs = @input.split(' -> ').map{|c| c.split(',')}
-        
-        @x1 = parsed_inputs[0][0].to_i
-        @x2 = parsed_inputs[1][0].to_i
-        @y1 = parsed_inputs[0][1].to_i
-        @y2 = parsed_inputs[1][1].to_i
+  attr_accessor :x1, :x2, :y1, :y2
+  #
+  # @param [<Type>] input String input in format x1,y1 -> x2,y2
+  #
+  def initialize(input)
+    @input = input.strip
+    parsed_inputs = @input.split(" -> ").map { |c| c.split(",").map(&:to_i) }
+
+    @x1 = parsed_inputs[0][0]
+    @x2 = parsed_inputs[1][0]
+    @y1 = parsed_inputs[0][1]
+    @y2 = parsed_inputs[1][1]
+  end
+
+  #
+  # Iterator of coordinates that draw a line of the input
+  #
+  # @return [Iterator] iterator yields coordings of type Array: [x, y]
+  #
+  def coor_iter
+    @coords = []
+    if is_horizontal?
+      ([@x1, @x2].min..[@x1, @x2].max).each { |x| @coords << [x, @y1] }
+    elsif is_vertical?
+      ([@y1, @y2].min..[@y1, @y2].max).each { |y| @coords << [@x1, y] }
+    else
+      diagnol_length = (@y2 - @y1).abs
+
+      x_step = @x1 < @x2 ? 1 : -1
+      y_step = @y1 < @y2 ? 1 : -1
+
+      (0..diagnol_length).each do |offset|
+        @coords << [@x1 + x_step * offset, @y1 + y_step * offset]
+      end
     end
 
-    #
-    # Iterator of coordinates that draw a line of the input
-    #
-    # @return [Iterator] iterator yields coordings of type Array: [x, y]
-    #
-    def coor_iter
-        @coords = []
-        if self.is_horizontal?
-            ([@x1, @x2].min .. [@x1, @x2].max).each {|x| @coords << [x, @y1]}
-        elsif self.is_vertical?
-            ([@y1, @y2].min .. [@y1, @y2].max).each {|y| @coords << [@x1, y]}
-        else
-            diagnol_length = (@y2 - @y1).abs
+    @coords.each { |coord| yield coord }
+  end
 
-            x_step = @x1 < @x2 ? 1 : -1
-            y_step = @y1 < @y2 ? 1 : -1
+  #
+  # @return [Boolean ]
+  #
+  def is_horizontal?
+    @y1 == @y2
+  end
 
-            (0..diagnol_length).each do |offset|
-                @coords << [@x1 + x_step*offset, @y1+y_step*offset]
-            end
-        end
-
-        @coords.each {|coord| yield coord}
-    end
-
-
-    #
-    # @return [Boolean ]
-    #
-    def is_horizontal?
-        @y1 == @y2
-    end
-
-    #
-    # @return [Boolean ]
-    #
-    def is_vertical?
-        @x1 == @x2
-    end
+  #
+  # @return [Boolean ]
+  #
+  def is_vertical?
+    @x1 == @x2
+  end
 end
-
 
 class World
-    attr_accessor :vents
+  attr_accessor :vents
 
-    def initialize
-        @vents = Hash.new(0)
-    end
+  def initialize
+    @vents = Hash.new(0)
+  end
 
-    #
-    # Adds input to vent map
-    #
-    # @param [Input] 
-    #
-    # @return [void] 
-    #
-    def add_vent(input)
-        input.coor_iter do |coord|
-            @vents[coord] += 1
-        end
+  #
+  # Adds input to vent map
+  #
+  # @param [Input]
+  #
+  # @return [void]
+  #
+  def add_vent(input)
+    input.coor_iter do |coord|
+      @vents[coord] += 1
     end
+  end
 end
-
 
 world = World.new
-lines = File.readlines('input').map do |line|
+File.readlines("input").map do |line|
   input = Input.new(line)
   # uncomment below for part 1
-  #next unless input.is_horizontal? || input.is_vertical?
+  # next unless input.is_horizontal? || input.is_vertical?
   world.add_vent(input)
-  answer = world.vents.values.filter {|v| v > 1}.length
 end
 
-answer = world.vents.values.filter {|v| v > 1}.length
-
-puts answer
+puts world.vents.values.count { |v| v > 1 }
